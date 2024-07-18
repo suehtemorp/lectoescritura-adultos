b382fd4fb451795fec541339385201f501c7f6eb
Javier Solano Saltachin <javier.solanosaltachin@ucr.ac.cr>
2024-07-18 05:14:41 -0600

    Color palette template & hooks

---
+++ src/constants/Colors/DarkColors.ts
@@ -0,0 +1,93 @@
+// Dependencias
+// Tipos de colores según Material Design 
+import { MaterialPalette } from "@/shared/Colors/MaterialDesignColorTypes";
+
+// Colores de temas oscuros
+const Colors : MaterialPalette = {
+	Primary: {
+        Main: {
+            Main: "#fff",
+            Container: "#fff",
+
+            OnMain: "#fff",
+            OnContainer: "#fff",
+        },
+        Fixed: {
+            Main: "#fff",
+            Dim: "#fff",
+
+            OnMain: "#fff", 
+            OnVariant: "#fff",
+        },
+    },
+    Secondary: {
+        Main: {
+            Main: "#fff",
+            Container: "#fff",
+
+            OnMain: "#fff",
+            OnContainer: "#fff",
+        },
+        Fixed: {
+            Main: "#fff",
+            Dim: "#fff",
+
+            OnMain: "#fff", 
+            OnVariant: "#fff",
+        },
+    },
+    Tertiary: {
+        Main: {
+            Main: "#fff",
+            Container: "#fff",
+
+            OnMain: "#fff",
+            OnContainer: "#fff",
+        },
+        Fixed: {
+            Main: "#fff",
+            Dim: "#fff",
+
+            OnMain: "#fff", 
+            OnVariant: "#fff",
+        },
+    },
+
+    Error: {
+        Main: "#fff",
+        Container: "#fff",
+
+        OnMain: "#fff",
+        OnContainer: "#fff",
+    },
+
+    Surface: {
+        Main: "#fff",
+        Dim: "#fff",
+        Bright: "#fff",
+
+        Outline: "#fff",
+        OutlineVariant: "#fff",
+
+        OnMain: "#fff",
+        OnVariant: "#fff",
+
+        ContainerLowest: "#fff",
+        ContainerLow: "#fff",
+        Container: "#fff",
+        ContainerHigh: "#fff",
+        ContainerHighest: "#fff",
+    },
+
+    Inverse: {
+        Primary: "#fff",
+        Surface: "#fff",
+
+        OnSurface: "#fff", 
+    },
+
+    Scrim: "#fff",
+    Shadow: "#fff", 
+};
+
+export default Colors;
---
+++ src/constants/Colors/ThemedColors.ts
@@ -0,0 +1,14 @@
+// Dependencias
+// Tipos de paletas de colores según Material Design 
+import { ThemedPalette } from "@/shared/Colors/ColorTypes";
+
+// Colores brillantes y oscuros
+import LightColors from "@/constants/Colors/LightColors";
+import DarkColors from "@/constants/Colors/DarkColors";
+
+const ThemedColors : ThemedPalette = {
+	light: LightColors,
+	dark: DarkColors,
+};
+
+export default ThemedColors;
