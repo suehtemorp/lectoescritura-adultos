ea509ad563e615d2ceabee1a7d8c08e0cfdac26d
Javier Solano Saltachin <javier.solanosaltachin@ucr.ac.cr>
2025-02-12 23:20:03 -0600

    Added main menu buttons

--- src/app/(root)/(menu)/Home.tsx
+++ src/app/(root)/(menu)/Home.tsx
@@ -1,22 +1,49 @@
-import { View, StyleSheet, Text } from "react-native";
-import { Image } from "expo-image";
-import AppBanner from "@/assets/images/book_icon.png"
+import { View, StyleSheet, Text, TouchableOpacity, ColorValue } from "react-native";
+
+// Controlador de navegación
+import { router } from 'expo-router';
+
+// Posibles tipos de niveles
+import { LevelClass } from '@/shared/Levels/LevelTypes';
 
 export default function Home() {
 return ( 
     <View style={styles.background}> 
-        <Image
-            source={AppBanner}
-            onError={(error) => {
-                console.error("Error loading image:", error);
-            }}
-
-            contentFit="contain"
-            contentPosition="center"
-            style={styles.image}
-        />
+        <View style={styles.optionGrid}>
+            {/* TODO: Use named colors */}
+            <LevelTypeButton levelType={"Vowel"} fill={"#09B930"} outline={"#025B74"} />
+            <LevelTypeButton levelType={"SimpleConsonant"} fill={"#5A92FE"} outline={"#275CC3"}/>
+            <LevelTypeButton levelType={"AmbiguousConsonant"} fill={"#EE5A9B"} outline={"#BA1F63"}/>
+        </View>
     </View>
 );
+}
+
+// Botón para seleccionar nivel
+const LevelTypeButton = (props: {
+    levelType : LevelClass, fill: ColorValue, outline: ColorValue}) => {
+    return (
+        <TouchableOpacity style={styles.iconButton}
+            onPress={() => {
+                router.navigate(`/SelectLevel/${props.levelType}`);
+            }}
+        >
+            <View style={[
+                styles.buttonContainer, 
+                {backgroundColor : props.fill, borderColor: props.outline}
+            ]}>
+                <Text style={styles.buttonText}> 
+                    {
+                        props.levelType === "Vowel" ? "Vocales" : (
+                            props.levelType === "SimpleConsonant" ?
+                            "Consonantes simples" :
+                            "Consonantes ambiguas"
+                        )
+                    }
+                </Text>
+            </View>
+        </TouchableOpacity>
+    );
 }
 
 // Estilos
@@ -27,10 +54,48 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignContent: "center",
         alignItems: "center",
-        backgroundColor: "#f9faef",
+        backgroundColor: "#FFEDD3", // TODO: Add named color
     },
     image : {
         width: "70%",
         height: "70%",
+    },
+    optionGrid : {
+        width: "90%",
+        height: "70%",
+        rowGap: "10%",
+        columnGap: "10%",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+        marginHorizontal: "auto",
+    },
+    iconButton: {
+        width: "30%",
+        height: "45%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    buttonContainer: {
+        width: "100%",
+        height: "100%",
+        borderWidth: 5,
+        borderRadius: 35,
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+        overflow: 'hidden',
+        borderColor: "black",
+        backgroundColor: "white",
+        elevation: 20
+    },
+    buttonText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white",
+        fontFamily: "arial",
+        textAlign: "center",
     }
 });
