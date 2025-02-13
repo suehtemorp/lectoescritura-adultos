46bd4870e92fae6841fc4b8f1f284b68b3f2e61f
Javier Solano Saltachin <javier.solanosaltachin@ucr.ac.cr>
2025-02-12 22:40:17 -0600

    Added horizontal layout lock

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,9 +6,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
 
 // Cliente de consultas y mutaciones a ser usado en toda la app
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
+
+// Orientación de la patalla
+import * as ScreenOrientation from "expo-screen-orientation"; 
 
 export default function Layout() {
+  // Ajustar orientación de pantalla a horizontal si el dispositivo lo soporta 
+  ScreenOrientation
+    .supportsOrientationLockAsync(ScreenOrientation.OrientationLock.LANDSCAPE)
+    .then(async (landscapeSupported) => {
+      console.debug("Landscape orientation is " + (!landscapeSupported ? "NOT " : "") + "SUPPORTED");
+      if (landscapeSupported) {
+        console.debug("Adjusting orientation to landscape");
+        await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+      }
+    });
+
   return (
     // Inyectar cliente como dependencia a nodos
     <QueryClientProvider client={queryClient}>
