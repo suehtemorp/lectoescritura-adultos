39db1e6875454d3e748e0038503960439521145e
Javier Solano Saltachin <javier.solanosaltachin@ucr.ac.cr>
2025-03-31 17:18:01 -0600

    Added audio description to level select page

--- src/app/(root)/(menu)/LevelSelect.tsx
+++ src/app/(root)/(menu)/LevelSelect.tsx
@@ -1,14 +1,22 @@
 // UI de React Native
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ColorValue } from 'react-native';
 
 // Navegación y parámetros de URL
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, usePathname } from 'expo-router';
 
 // Botón de nivel
 import LevelButton from '@/components/Levels/LevelButton';
 
 // Tipos de niveles
 import {LevelClass} from "@/shared/Levels/LevelTypes"
+
+// Contexto de encabezado de puntaje
+import { MainLayoutContext } from '@/components/Navigation/MainLayoutContext';
+import { useContext, useEffect } from 'react';
+import { useProgress } from '@/shared/Score/UserProgress';
+
+// Audios de ayuda
+import HelpAudios from '@/constants/HelpAudios';
 
 export default function SelectLevel () {
     // Obtener tipo de nivel a usar en selección
@@ -18,17 +26,38 @@ export default function SelectLevel () {
         throw Error("Tipo de nivel indefinido en selección de niveles");
     }
 
+    // Obtener progreso de usuario en nivel
+    const progress = useProgress();
+    const levelIndex : number = progress.isLoading || progress.isError ?
+        0 : progress.data?.levels_completed[levelType]?? 0;
+
+    // Actualizar audio de ayuda y color cuando ingresa a la página según tipo
+    // de nivel y progreso
+    const mainLayoutContext = useContext(MainLayoutContext);
+    const path = usePathname();
+    useEffect(() => {
+        if (levelIndex > 5 || levelIndex < 0) {
+            console.error("Invalid level index of " + levelIndex);
+        } else if (mainLayoutContext) {
+            mainLayoutContext.setMainLayoutInformation({
+                theme: levelType,
+                // TODO: Make level index type safe
+                helpAudio: levelIndex === 5 ? "Level_Complete" : 
+                    `Level_${levelIndex+1}` as keyof typeof HelpAudios
+            })
+        }
+    }, [path, progress.data?.levels_completed]);
+
     // Mostrar botones para niveles
     // TODO: Cargar niveles de almacenamiento, escoger colores programáticamente
     return (
         <View style={styles.background}>
             <View style={styles.iconGrid}>
                 <LevelButton levelIndex={1} levelType={levelType} isBlocked={false} bgColor={"#7bd785"} />
-                <LevelButton levelIndex={2} levelType={levelType} isBlocked={false} bgColor={"#d7b27b"} />
-                <LevelButton levelIndex={3} levelType={levelType} isBlocked={false} bgColor={"#d7917b"} />
-                <LevelButton isBlocked={true} />
-                <LevelButton isBlocked={true} />
-                <LevelButton isBlocked={true} />
+                <LevelButton levelIndex={2} levelType={levelType} isBlocked={levelIndex < 1} bgColor={"#d7b27b"} />
+                <LevelButton levelIndex={3} levelType={levelType} isBlocked={levelIndex < 2} bgColor={"#d7917b"} />
+                <LevelButton levelIndex={4} levelType={levelType} isBlocked={levelIndex < 3} bgColor={"#d7917b"} />
+                <LevelButton levelIndex={5} levelType={levelType} isBlocked={levelIndex < 4} bgColor={"#d7917b"} />
             </View>
         </View>
     );
@@ -45,9 +74,9 @@ const styles = StyleSheet.create({
         backgroundColor: "#cdeda3",
     },
     iconGrid : {
-        width: "90%",
-        height: "70%",
-        marginBottom: "30%",
+        width: "100%",
+        height: "100%",
+        // marginBottom: "30%",
         rowGap: 10,
         columnGap: 10,
         justifyContent: "center",
@@ -56,3 +85,11 @@ const styles = StyleSheet.create({
         flexWrap: "wrap",
     },
 });
+
+
+// Tintes de iconos según tipo de nivel
+const ThemeBorder : { [K in LevelClass]: ColorValue } = {
+    Vowel: "#009621",
+    SimpleConsonant: "#275CC3",
+    AmbiguousConsonant: "#BA1F63",
+};
